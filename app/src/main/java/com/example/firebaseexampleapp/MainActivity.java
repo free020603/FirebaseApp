e2c36b214ab7372a7fb559c8d2b6aaff5ec63e29
free020603 <34528924+free020603@users.noreply.github.com>
2020-01-14 11:51:46 -0600

    git

--- app/src/main/java/com/example/firebaseexampleapp/MainActivity.java
+++ app/src/main/java/com/example/firebaseexampleapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.firebaseexampleapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,7 +13,12 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +28,14 @@ public class MainActivity extends AppCompatActivity {
     private int dateDay;
     private int dateYear;
 
+    FirebaseDatabaseHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new FirebaseDatabaseHelper();
 
         //  Video to learn basic access to CalendarView Data
         //  https://www.youtube.com/watch?v=WNBE_3ZizaA
@@ -32,10 +43,10 @@ public class MainActivity extends AppCompatActivity {
         CalendarView calendarView = findViewById(R.id.eventCalendarDate);
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
 
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
                                                  @Override
                                                  public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
-                                                     dateSelected =  (month + 1) + "/" + day + "/" + year;
+                                                     dateSelected = (month + 1) + "/" + day + "/" + year;
                                                      dateYear = year;
                                                      dateMonth = month + 1;
                                                      dateDay = day;
@@ -51,14 +62,15 @@ public class MainActivity extends AppCompatActivity {
         String eventName = eventNameET.getText().toString();
 
         // verify there is a name and date
-        if (eventName.length() == 0 ) {
+        if (eventName.length() == 0) {
             Toast.makeText(MainActivity.this, "Please enter name", Toast.LENGTH_SHORT).show();
-        }
-        else if (dateSelected.equals("No date chosen")) {
+        } else if (dateSelected.equals("No date chosen")) {
             Toast.makeText(MainActivity.this, "Please select Date", Toast.LENGTH_SHORT).show();
-        }
-        else {
-           Log.i(TAG, "Trying to add: " + eventName + ", " + dateSelected);
+        } else {
+            Log.i(TAG, "Trying to add: " + eventName + ", " + dateSelected);
+            Event newEvent = new Event(eventName, dateSelected, dateYear, dateMonth, dateDay);
+            eventNameET.setText("");
+            dbHelper.addEvent(newEvent);
         }
     }
 
@@ -66,14 +78,22 @@ public class MainActivity extends AppCompatActivity {
      * This method will be called to minimize the on screen keyboard in the Activity
      * When we get the current view, it is the view that has focus, which is the keyboard
      * Credit - Found by Ram Dixit, 2019
-     *
+     * <p>
      * Source:  https://www.youtube.com/watch?v=CW5Xekqfx3I
      */
     private void closeKeyboard() {
         View view = this.getCurrentFocus();     // view will refer to the keyboard
-        if (view != null ){                     // if there is a view that has focus
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (view != null) {                     // if there is a view that has focus
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
+    }
+
+
+    // in EditEventActivity.java
+    public void onRetrieve(View v) {
+        Intent intent = new Intent(MainActivity.this, DisplayEventsActivity.class);
+        intent.putExtra("events", dbHelper.getEventArrayList());
+        startActivity(intent);
     }
 }
